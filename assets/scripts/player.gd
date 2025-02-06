6e7d931dd570a931254cc00cfb06cf7083c3f1d3
psychotronixx <sabo.patrik1@gmail.com>
2025-02-06 23:35:36 +0100

    added player animation (NOT FINISHED)

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 5000.0
-const GRAVITY = 1000.0
-const JUMP_VELOCITY = 20000.0
+@export var SPEED = 5000.0
+@export var GRAVITY = 1000.0
+@export var JUMP_VELOCITY = 20000.0
 
 var current_planet: StaticBody2D = null
 var velocity_jump: Vector2 = Vector2.ZERO
 var velocity_gravity: Vector2 = Vector2.ZERO
+var jumped = false
 
 
 func _physics_process(delta: float) -> void:
@@ -24,6 +25,12 @@ func _physics_process(delta: float) -> void:
 		if $Grounded.is_colliding():
 			velocity_jump = Vector2.ZERO
 			velocity_gravity = Vector2.ZERO
+			if not $Graphics.is_playing():
+				$Graphics.play()
+				
+			if jumped and $Graphics.animation != "on_fall":
+				$Graphics.animation = "on_fall"
+			
 			# JUMP
 			if Input.is_action_just_pressed("move_Jump") and velocity_jump == Vector2.ZERO:
 				velocity_jump = velocity_to_planet * JUMP_VELOCITY
@@ -35,9 +42,19 @@ func _physics_process(delta: float) -> void:
 		var direction := Input.get_axis("move_L", "move_R")
 		if direction:
 			velocity_move += (velocity_up) * direction * SPEED
-			
 			$Graphics.flip_h = direction == -1
-		
+			if velocity_jump != Vector2.ZERO:
+				$Graphics.animation = "jump"
+				jumped = true
+			else:
+				$Graphics.animation = "run"
+		else:
+			if velocity_jump != Vector2.ZERO:
+				$Graphics.animation = "jump"
+				jumped = true
+			elif $Graphics.animation != "on_fall":
+				$Graphics.animation = "idle"
+				
 		velocity = (velocity_gravity + velocity_jump + velocity_move) * delta
 	move_and_slide()
 
@@ -46,3 +63,9 @@ func _on_planet_detect_area_entered(area: Area2D) -> void:
 	
 func _on_planet_detect_area_exited(area: Area2D) -> void:
 	current_planet = null
+
+
+func _on_graphics_animation_finished() -> void:
+	if $Graphics.animation == "on_fall":
+		jumped = false
+		$Graphics.animation = "idle"
