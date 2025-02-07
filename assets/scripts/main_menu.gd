1632b1a908ed461dbf118870f0a3ef1af322c0a7
psychotronixx <sabo.patrik1@gmail.com>
2025-02-07 10:02:20 +0100

    changed name VulkanoParticles, updated MainMenu

--- assets/scripts/main_menu.gd
+++ assets/scripts/main_menu.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+var particles
+
+
+func _ready():
+	particles = $Vulkano.get_node_or_null("VulkanoParticles")
+	particles.lifetime = 1.5
+
+func _physics_process(delta: float) -> void:
+	$AnimatedSprite2D.play("idle")
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/scenes/main.tscn")
