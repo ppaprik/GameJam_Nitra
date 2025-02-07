e307d032729e81821ed9283eab6c5300f1a1597a
psychotronixx <sabo.patrik1@gmail.com>
2025-02-07 14:57:03 +0100

    updated MainMenu

--- assets/scripts/main_menu.gd
+++ assets/scripts/main_menu.gd
@@ -1,17 +1,28 @@
 extends Node2D
 
 var particles
+var is_quit_hovered: bool = false  # Track if quit button is hovered
 
+@onready var menu_confirm: AudioStreamPlayer2D = $Menu_confirm
+@onready var menu_music: AudioStreamPlayer2D = $Menu_music
 
 func _ready():
 	particles = $Vulkano.get_node_or_null("VulkanoParticles")
-	particles.lifetime = 1.5
+	if particles:
+		particles.lifetime = 1.5  # Ensure particles exist before modifying
+	menu_music.play()
 
 func _physics_process(delta: float) -> void:
 	$AnimatedSprite2D.play("idle")
 
 func _on_start_pressed() -> void:
+	menu_confirm.play()
 	get_tree().change_scene_to_file("res://assets/scenes/main.tscn")
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+
+func _on_start_mouse_entered() -> void:
+	menu_confirm.play()
+
+
+func _on_quit_mouse_entered() -> void:
+		menu_confirm.play()
