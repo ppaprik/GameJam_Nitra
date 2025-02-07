13065bda097b7b80031a4e5797738472c09c5387
psychotronixx <sabo.patrik1@gmail.com>
2025-02-07 16:52:15 +0100

    finito

--- assets/scripts/boss_music.gd
+++ assets/scripts/boss_music.gd
@@ -6,5 +6,5 @@ func _on_body_entered(body: Node2D) -> void:
 		Global.boss_music = true
 
 func _physics_process(delta: float) -> void:
-	if get_parent().get_node("../Graphics").animation == "die":
+	if $"../Graphics".animation == "die":
 		get_tree().change_scene_to_file("res://assets/scenes/win.tscn")
