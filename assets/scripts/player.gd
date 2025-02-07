905c10c3a6f34c807e1ec97cfd36b6b6b37c7bea
psychotronixx <sabo.patrik1@gmail.com>
2025-02-07 04:21:26 +0100

    updated player animation

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -8,12 +8,14 @@ extends CharacterBody2D
 var current_planet: StaticBody2D = null
 var velocity_jump: Vector2 = Vector2.ZERO
 var velocity_gravity: Vector2 = Vector2.ZERO
-var jumped = false
 
 var direction_to_planet = null
 var direction_up = null
 var velocity_to_planet = null
 var velocity_up = null
+
+var jumped = false
+var jump_finished = false
 
 
 func _physics_process(delta: float) -> void:
@@ -38,15 +40,12 @@ func _physics_process(delta: float) -> void:
 			velocity_jump = Vector2.ZERO
 			velocity_gravity = Vector2.ZERO
 			
-			if not $Graphics.is_playing():
-				$Graphics.play()
-			
 			# JUMP
 			if Input.is_action_just_pressed("move_Jump") and velocity_jump == Vector2.ZERO:
 				velocity_jump = velocity_to_planet * JUMP_VELOCITY
 		else:
 			velocity_gravity += velocity_to_planet * -1 * GRAVITY
-
+			
 		#--------------------------------------------------
 		# MOVE
 		var velocity_move = Vector2.ZERO
@@ -55,20 +54,20 @@ func _physics_process(delta: float) -> void:
 			velocity_move += (velocity_up) * direction * SPEED
 			$Graphics.flip_h = direction == -1
 		
-		if jumped:
-			$Graphics.animation = "on_fall"
-		elif (velocity_jump != Vector2.ZERO and direction) or velocity_jump != Vector2.ZERO:
-			$Graphics.animation = "jump"
-			#print($Graphics.frame_progress)
+		#--------------------------------------------------
+		# ANIMATIONS
+		if jump_finished:
+			$Graphics.play("on_fall")
+		elif Input.is_action_just_pressed("move_Jump") or jumped:
+			jumped = true
+			$Graphics.play("jump")
 		elif direction:
-			$Graphics.animation = "run"
+			$Graphics.play("run")
 		else:
-			$Graphics.animation = "idle"
+			$Graphics.play("idle")
 		
 		velocity = (velocity_gravity + velocity_jump + velocity_move) * delta
 		
-		#print($Graphics.get_fra)
-
 	move_and_slide()
 
 
@@ -82,6 +81,7 @@ func _on_planet_detect_area_exited(_area: Area2D) -> void:
 
 func _on_graphics_animation_finished() -> void:
 	if $Graphics.animation == "jump":
-		jumped = true
-	if $Graphics.animation == "on_fall":
+		jump_finished = true
 		jumped = false
+	if $Graphics.animation == "on_fall":
+		jump_finished = false
